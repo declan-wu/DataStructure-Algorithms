6edb55bb2f468973bf493bd799f9a1dea318e096
Declan Wu <wus92@mcmaster.ca>
2019-03-12 22:01:00 -0400

    add dfs and bfs

---
+++ Graphing/BFS.py
@@ -0,0 +1,54 @@
+
+def bfs(graph, start):
+    visited = []
+    queue = [start]
+
+    while queue:
+        vertex = queue.pop(0)
+        if vertex not in visited:
+            visited.append(vertex)
+            queue.extend(set(graph.nodes[vertex].getConnections()) - set(visited))
+            
+    return visited
+
+def bfs_paths(graph, start, goal):
+    result = []
+    queue = [(start, [start])]
+
+    while queue:
+        (vertex, path) = queue.pop(0)
+        for nxt in set(graph.nodes[vertex].getConnections()) - set(path):
+            if nxt == goal:
+                result.append(path + [nxt]) # could use yield path + [nxt]
+            else:
+                queue.append((nxt, path + [nxt]))
+
+    return result
+
+def shortest_path(graph, start, goal):
+    try:
+        return (bfs_paths(graph, start, goal))[0]
+    except StopIteration:
+        return None
+
+
+g = Graph() 
+g.add_edge(0, 1) 
+g.add_edge(0, 2) 
+g.add_edge(1, 2) 
+g.add_edge(1, 5) 
+g.add_edge(2, 3) 
+g.add_edge(3, 3) 
+g.add_edge(3, 5)
+
+print(shortest_path(g, 0, 5))
+
+
+
+
+
+
+
+
+
+
---
+++ Graphing/DFS.py
@@ -0,0 +1,53 @@
+# mark the current vertex as being visited
+# explore each adjecent vertex that is not included in the visited set
+
+from .graph import Graph 
+
+def dfs(graph, start):
+    visited = []
+    stack = [start]
+
+    while stack:
+        vertex = stack.pop()
+        if vertex not in visited:
+            visited.append(vertex)
+            stack.extend(set(graph.nodes[vertex].getConnections()) - set(visited))   #set difference
+            
+    return visited 
+
+def dfs_paths(graph, start, goal):
+    result = []
+    stack = [(start, [start])]
+
+    while stack:
+        (vertex, path) = stack.pop()
+        for nxt in set(graph.nodes[vertex].getConnections()) - set(path):
+            if nxt == goal:
+                result.append(path + [nxt]) # could use yield path + [nxt]
+            else:
+                stack.append((nxt, path + [nxt]))
+
+    return result
+
+
+g = Graph() 
+g.add_edge(0, 1) 
+g.add_edge(0, 2) 
+g.add_edge(1, 2) 
+g.add_edge(2, 0) 
+g.add_edge(2, 3) 
+g.add_edge(3, 3) 
+
+# print(dfs(g,2))
+print(dfs_paths(g, 0, 2))
+
+
+
+
+
+
+
+
+
+
+
